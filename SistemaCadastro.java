95fd8d888189fc06aff5d737bfd5a04d71be7882
Monique da Silva <moniquedasilva2005@gmail.com>
2025-07-03 19:49:00 -0300

    classe cadastro dos alunos 📝

--- SistemaCadastro.java
+++ SistemaCadastro.java
@@ -1,15 +1,19 @@
 import java.util.ArrayList;
 
 public class SistemaCadastro {
-    private ArrayList <Aluno> list;
+    private ArrayList <Aluno> list; //variável list que é uma lista de objetos do tipo Aluno
 
+    //construtor
     public SistemaCadastro(){
         list = new ArrayList<>();
     }
 
+    //metódo de adicionar alunos na lista
     public void addAluno(Aluno aluno){
         list.add(aluno);
     }
+    
+    //metódo de listar todos os alunos que estão na lista 
     public void listarAlunos(){
         System.out.println("Lista de alunos: ");
         for (Aluno aluno : list){
@@ -17,12 +21,13 @@ public class SistemaCadastro {
         }
     }
 
+    //metódo para buscar algum aluno pelo número de sua matrícula
     public Aluno buscarAluno(int matricula){
-        for(Aluno aluno : list){
+        for(Aluno aluno : list){ //for-each: (tipo, nomeVariável, lista)
             if(aluno.getMatricula() == matricula){
                 return aluno;
             }
         }
-        return null;
+        return null; //caso não encontrado
     }
 }
